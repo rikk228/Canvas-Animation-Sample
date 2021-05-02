0f51aa9a7ad370b35c53c3cf8f0d253c39786ea2
Riccardo Fedrizzi <riccardo.fedrizzi@marconirovereto.it>
2021-05-02 20:26:18 +0200

    Added circle, shape selection

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-let playbackBTN = document.querySelector('#playback');
+const playbackBTN = document.querySelector('#playback');
 playbackBTN.addEventListener('click', move);
 let isPlay = false;
 let interval;
 
-let resetBTN = document.querySelector('#reset');
+const resetBTN = document.querySelector('#reset');
 resetBTN.addEventListener('click', reset)
 
-const canvas = document.querySelector('canvas');
+const customPropertyBTN = document.querySelector('#apply');
+customPropertyBTN.addEventListener('click', customProperty);
+
+const shapeINP = document.querySelector('#shape');
+
+let shape = 'square';
+
+let canvas = document.querySelector('canvas');
 
 let context = canvas.getContext('2d');
 
@@ -16,22 +23,54 @@ let width = 50;
 let height = 50;
 let radius = 25;
 
-let speed = {
-    x: -3,
-    y: 2
+
+let speed;
+let coords;
+
+const fps = 60;
+const ms = 1000;
+const refreshRate = ms/fps;
+
+function assignBaseCoord() {
+
+    const baseCoords = {
+        circle: {
+            y: (canvas.height/2),
+            x: (canvas.width/2)
+        },
+        y: (canvas.height/2)-(height/2),
+        x: (canvas.width/2)-(width/2)
+    }
+    
+    coords = baseCoords;
+    
 }
 
-let pos = {
-    circle: {
-        y: (canvas.height/2)-(radius/2),
-        x: (canvas.width/2)-(radius/2)
-    },
-    y: (canvas.height/2)-(height/2),
-    x: (canvas.width/2)-(width/2)
-}
+function assignBaseSpeed() {
+    const baseSpeed = {
+        x: -3,
+        y: 2
+    }
 
+    speed = baseSpeed;
+}
 function drawSquare(x, y, width, height) {
     context.fillRect(x, y, width, height);
+}
+
+function drawCircle(x, y, radius) {
+    context.beginPath();
+    context.arc(x, y, radius, 0, 2*Math.PI);
+    context.fill();
+    context.closePath();
+}
+
+function basicSquare() {
+    context.fillRect(coords.x, coords.y, width, height);
+}
+
+function basicCircle() {
+    drawCircle(coords.circle.x, coords.circle.y, radius);
 }
 
 function clearCanvas() {
@@ -40,48 +79,78 @@ function clearCanvas() {
 
 function moveSquare() {
     clearCanvas();
-    drawSquare(pos.x, pos.y, width, height);
-    pos.x += speed.x;
-    pos.y += speed.y;
-    if(pos.x+width > canvas.width) speed.x = -speed.x;
-    if(pos.x < 0) speed.x = -speed.x;
-    if(pos.y+height > canvas.height) speed.y = -speed.y;
-    if(pos.y < 0) speed.y = -speed.y;
+    drawSquare(coords.x, coords.y, width, height);
+    coords.x += speed.x;
+    coords.y += speed.y;
+    if(coords.x + width > canvas.width) speed.x = -speed.x;
+    else if(coords.x < 0) speed.x = -speed.x;
+    if(coords.y + height > canvas.height) speed.y = -speed.y;
+    else if(coords.y < 0) speed.y = -speed.y;
 }
 
-function move() {
+function moveCircle() {
+    clearCanvas()
+    drawCircle(coords.circle.x, coords.circle.y, radius);
+    coords.circle.x += speed.x;
+    coords.circle.y += speed.y;
+    if(coords.circle.x + radius > canvas.width) speed.x = -speed.x;
+    else if(coords.circle.x - radius < 0) speed.x = -speed.x;
+    if(coords.circle.y + radius > canvas.height) speed.y = -speed.y;
+    else if(coords.circle.y - radius < 0) speed.y = -speed.y;
+}
+
+function move(evt) {
     
     isPlay = !isPlay;
-    
+    clearInterval(interval);
     if(isPlay) {
-        interval = setInterval(moveSquare, 16); 
+        if(shape === 'square') interval = setInterval(moveSquare, refreshRate); 
+        if(shape === 'circle') interval = setInterval(moveCircle, refreshRate);
         playbackBTN.innerHTML = '<i class="fas fa-pause"></i>';
-        return; 
+        return;
     }
     playbackBTN.innerHTML = '<i class="fas fa-play"></i> ';
-    clearInterval(interval);
+   
 }
 
-function reset() {
+function reset(evt, customShape, customSpeed, customCoords) {
+
     isPlay = false;
     clearInterval(interval);
     playbackBTN.innerHTML = '<i class="fas fa-play"></i> ';
     clearCanvas();
 
-    let basePos = {
-        circle: {
-            y: (canvas.height/2)-(radius/2),
-            x: (canvas.width/2)-(radius/2)
-        },
-        y: (canvas.height/2)-(height/2),
-        x: (canvas.width/2)-(width/2)
-    }
+    assignBaseCoord();
+    assignBaseSpeed();
 
-    let baseSpeed = {
-        x: -3,
-        y: 2
-    }
+    if(customSpeed) speed = customSpeed;
+    if(customCoords) coords = customCoords;
 
-    speed = baseSpeed;
-    pos = basePos;
+
+
+    if(shape === 'square' || customShape === 'square') {
+        basicSquare();
+    }
+    if(shape === 'circle' || customShape === 'circle') {
+        basicCircle();
+    }
+}
+
+function customProperty(evt) {
+    shape = shapeSelection();
+    reset(null, shape);
+}
+
+function shapeSelection() {
+    return shapeINP.value;
+}
+
+window.onload = load;
+
+function load() {
+    shape = shapeSelection();
+    clearCanvas();
+    assignBaseCoord();
+    assignBaseSpeed();
+    basicSquare();
 }
